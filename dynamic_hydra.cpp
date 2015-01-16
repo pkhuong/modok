4d58efd8178d9b405d003fe92a82bf13d5865112
Paul Khuong <pkhuong@appnexus.com>
2015-01-16 10:06:52 -0500

    Comments

--- dynamic_hydra.cpp
+++ dynamic_hydra.cpp
@@ -6,6 +6,26 @@
 
 namespace dynamic {
 
+/*
+ * Quoth "Fast Distributed Coordinate Descent for Non-Strongly Convex Losses"
+ *    (http://arxiv.org/abs/1405.5300v2), Algorithm 1.
+ *
+ * t^i_k = arg min_t f'_i(\theta^2_k u_k + z_k)t + s\Theta_2 D_ii/2\tau t^2 + [box constraint]
+ *
+ * f'_i(\theta^2_k u_k + z_k) = \sum_{j \in rows} A_ji violation_j
+ *                            = \sum_{j\in \rows} A_ji (\theta^2_k ru_j + rz_j)
+ *
+ * We're approximating the problem in one dimension with a derivative + fixed quadratic term.
+ * Let's solve that alternate problem (find the zero and clamp) and let t_i be the step taken.
+ *
+ * Next step is to update z and u:
+ *
+ *   z^i += t^i, u^i += (s/\tau\theta_k - 1/\theta^2_k) t^i
+ *
+ * and update rz = Az - b and ru = Au.
+ *
+ * I simplified some of the arithmetic with sample_rate = \tau/s
+ */
 static void
 coordinate_descent(struct state_t &state, const column_t &variable)
 {
@@ -95,6 +115,32 @@ hydra(struct state_t &state, size_t n)
 	return state.get_x();
 }
 
+/*
+ * Same paper, Section 4.
+ *
+ * \omega_j is the number of non-zeros in row j (# vars that interact w/ j).
+ * \omega^\prime_j is the number of *processors* that interact with row j.
+ *
+ * \tau is # random choice per iteration per processor, s is # variables/processor
+ *
+ * s_1 = max{1, s - 1}
+ *
+ * For constraint j:
+ *
+ * alpha_1 = 1 + (\tau - 1)(\omega_j - 1)/s_1
+ *
+ * alpha_2 = (\tau/s - (\tau - 1)/s_1) (\omega^\prime_j - 1)/\omega^\prime_j \omega_j
+ *
+ * There's obviously a lot of redundancy here.
+ *
+ * Finally, alpha_j = alpha_1 + alpha_2.
+ *
+ * D_ii = \sum_j \alpha_j \A^2_{ji}.
+ *
+ * the idea is that the more this variable interacts with other
+ * variables (via constraints), the more conservative we want each
+ * step to be.
+ */
 void
 update_steps(struct state_t &state)
 {
